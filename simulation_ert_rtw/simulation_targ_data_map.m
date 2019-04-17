b2d8e5e6611d8500b9839d3747c43f8b31e458e9
Ron <rpicard92@gmail.com>
2019-04-17 15:51:11 -0500

    updates

--- simulation_ert_rtw/simulation_targ_data_map.m
+++ simulation_ert_rtw/simulation_targ_data_map.m
@@ -28,16 +28,40 @@
     ;%
     ;% Auto data (pixsu5o10a)
     ;%
-      section.nData     = 2;
-      section.data(2)  = dumData; %prealloc
+      section.nData     = 8;
+      section.data(8)  = dumData; %prealloc
       
 	  ;% pixsu5o10a.DigitalRead_SampleTime
 	  section.data(1).logicalSrcIdx = 0;
 	  section.data(1).dtTransOffset = 0;
 	
-	  ;% pixsu5o10a.Constant1_Value
+	  ;% pixsu5o10a.Constant2_Value
 	  section.data(2).logicalSrcIdx = 1;
 	  section.data(2).dtTransOffset = 1;
+	
+	  ;% pixsu5o10a.Constant3_Value
+	  section.data(3).logicalSrcIdx = 2;
+	  section.data(3).dtTransOffset = 2;
+	
+	  ;% pixsu5o10a.Constant1_Value
+	  section.data(4).logicalSrcIdx = 3;
+	  section.data(4).dtTransOffset = 3;
+	
+	  ;% pixsu5o10a.DataStoreMemory_InitialValue
+	  section.data(5).logicalSrcIdx = 4;
+	  section.data(5).dtTransOffset = 4;
+	
+	  ;% pixsu5o10a.DataStoreMemory1_InitialValue
+	  section.data(6).logicalSrcIdx = 5;
+	  section.data(6).dtTransOffset = 5;
+	
+	  ;% pixsu5o10a.DataStoreMemory2_InitialValue
+	  section.data(7).logicalSrcIdx = 6;
+	  section.data(7).dtTransOffset = 6;
+	
+	  ;% pixsu5o10a.DataStoreMemory3_InitialValue
+	  section.data(8).logicalSrcIdx = 7;
+	  section.data(8).dtTransOffset = 7;
 	
       nTotData = nTotData + section.nData;
       paramMap.sections(1) = section;
@@ -61,7 +85,7 @@
   ;%**************************
       
     nTotData      = 0; %add to this count as we go
-    nTotSects     = 1;
+    nTotSects     = 2;
     sectIdxOffset = 0;
     
     ;%
@@ -87,12 +111,23 @@
       section.nData     = 1;
       section.data(1)  = dumData; %prealloc
       
-	  ;% gmqz5bowkvs.bqqfsa3jy3
+	  ;% gmqz5bowkvs.bcbn3efdcy
 	  section.data(1).logicalSrcIdx = 0;
 	  section.data(1).dtTransOffset = 0;
 	
       nTotData = nTotData + section.nData;
       sigMap.sections(1) = section;
+      clear section
+      
+      section.nData     = 1;
+      section.data(1)  = dumData; %prealloc
+      
+	  ;% gmqz5bowkvs.bqqfsa3jy3
+	  section.data(1).logicalSrcIdx = 1;
+	  section.data(1).dtTransOffset = 0;
+	
+      nTotData = nTotData + section.nData;
+      sigMap.sections(2) = section;
       clear section
       
     
@@ -113,8 +148,8 @@
   ;%*******************
       
     nTotData      = 0; %add to this count as we go
-    nTotSects     = 4;
-    sectIdxOffset = 1;
+    nTotSects     = 5;
+    sectIdxOffset = 2;
     
     ;%
     ;% Define dummy sections & preallocate arrays
@@ -158,12 +193,24 @@
       dworkMap.sections(2) = section;
       clear section
       
-      section.nData     = 1;
-      section.data(1)  = dumData; %prealloc
+      section.nData     = 4;
+      section.data(4)  = dumData; %prealloc
       
-	  ;% nlkg0wdxnrm.bzyq1zbjhs.LoggedData
+	  ;% nlkg0wdxnrm.ashvbj3oyn
 	  section.data(1).logicalSrcIdx = 2;
 	  section.data(1).dtTransOffset = 0;
+	
+	  ;% nlkg0wdxnrm.flyj51aq4s
+	  section.data(2).logicalSrcIdx = 3;
+	  section.data(2).dtTransOffset = 1;
+	
+	  ;% nlkg0wdxnrm.gqxlow51xt
+	  section.data(3).logicalSrcIdx = 4;
+	  section.data(3).dtTransOffset = 2;
+	
+	  ;% nlkg0wdxnrm.mqbbpc1j4u
+	  section.data(4).logicalSrcIdx = 5;
+	  section.data(4).dtTransOffset = 3;
 	
       nTotData = nTotData + section.nData;
       dworkMap.sections(3) = section;
@@ -172,16 +219,31 @@
       section.nData     = 2;
       section.data(2)  = dumData; %prealloc
       
-	  ;% nlkg0wdxnrm.fu1xq2yfx1
-	  section.data(1).logicalSrcIdx = 3;
+	  ;% nlkg0wdxnrm.bzyq1zbjhs.LoggedData
+	  section.data(1).logicalSrcIdx = 6;
 	  section.data(1).dtTransOffset = 0;
 	
-	  ;% nlkg0wdxnrm.dni4tvjzax
-	  section.data(2).logicalSrcIdx = 4;
+	  ;% nlkg0wdxnrm.la12mnptly.LoggedData
+	  section.data(2).logicalSrcIdx = 7;
 	  section.data(2).dtTransOffset = 1;
 	
       nTotData = nTotData + section.nData;
       dworkMap.sections(4) = section;
+      clear section
+      
+      section.nData     = 2;
+      section.data(2)  = dumData; %prealloc
+      
+	  ;% nlkg0wdxnrm.fu1xq2yfx1
+	  section.data(1).logicalSrcIdx = 8;
+	  section.data(1).dtTransOffset = 0;
+	
+	  ;% nlkg0wdxnrm.dni4tvjzax
+	  section.data(2).logicalSrcIdx = 9;
+	  section.data(2).dtTransOffset = 1;
+	
+      nTotData = nTotData + section.nData;
+      dworkMap.sections(5) = section;
       clear section
       
     
@@ -210,8 +272,8 @@
   ;%
 
 
-  targMap.checksum0 = 3559592039;
-  targMap.checksum1 = 712787088;
-  targMap.checksum2 = 1300085884;
-  targMap.checksum3 = 3836832326;
+  targMap.checksum0 = 2904923480;
+  targMap.checksum1 = 2026979887;
+  targMap.checksum2 = 2776377970;
+  targMap.checksum3 = 2525795103;
 
